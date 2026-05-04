a06aa7b04bc48afc928a89a50fd4ad059ac3977d
braintop <braintop7@gmail.com>
2026-05-04 22:21:49 +0300

    Add

--- 18-4.5.2026-callback-inorderfinctions/15-disco2/app.js
+++ 18-4.5.2026-callback-inorderfinctions/15-disco2/app.js
@@ -1,6 +1,10 @@
 counter = 0; 
 setInterval(() => {
   let colors = ["red", "green", "blue", "yellow", "purple"];
-  document.body.style.backgroundColor = colors[counter % colors.length];
+  document.body.style.backgroundColor = colors[counter];
   counter++;
+  if(counter >= colors.length){
+    counter = 0;
+  }
+  
   }, 1000);
