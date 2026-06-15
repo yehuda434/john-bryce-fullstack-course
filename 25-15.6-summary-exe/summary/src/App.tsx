c331fb1af3dfab58cc215f4d0df7a8d0b04cca1f
braintop <braintop7@gmail.com>
2026-06-15 18:20:54 +0300

    Add

--- 25-15.6-summary-exe/summary/src/App.tsx
+++ 25-15.6-summary-exe/summary/src/App.tsx
@@ -1,7 +1,21 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Products from './Pages/Products/Products'
+import ProductDetails from './Pages/ProductDetails/ProductDetails'
+import Favorites from './Pages/Favorites/Favorites'
+import Navbar from './Components/Navbar/Navbar'
+import NotFound from './NotFound/NotFound'
 function App() {
   return (
     <>
-      <h1>Summary</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
     </>
   )
--- 25-15.6-summary-exe/summary/src/Components/Navbar/Navbar.tsx
+++ 25-15.6-summary-exe/summary/src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,14 @@
+import {NavLink} from 'react-router-dom' 
+import styles from './Navbar.module.css'
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={styles.navbar}>
+      <h2 className={styles.logo}>MiniS Store</h2>
+      <div className={styles.links}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Products</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </div>
     </div>
   )
 }
